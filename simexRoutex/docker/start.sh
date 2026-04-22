c7e604dcb7edbf9ad01fd3710bbf432ac2a77c17
Josep Guiu Sillés <jguius2021@cepnet.net>
2026-04-22 16:01:19 +0200

    Comment Despliegue

--- simexRoutex/docker/start.sh
+++ simexRoutex/docker/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-php-fpm -D
-sleep 2
-nginx -g "daemon off;"
+php-fpm -D #inicia motor php
+sleep 2 #timmer para deja php iniciarse
+nginx -g "daemon off;" #Inicia el nginx y lo mantiene
